e6d8518529c4d727e03cc0ccbc5694a6fb1f9d91
Jevaughn Mckenzie <jmckenzie1004@gmail.com>
2017-05-12 19:39:46 -0400

    Initial Commit

---
+++ GifViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GifViewer
+//
+//  Created by Jevaughn McKenzie on 5/12/17.
+//  Copyright © 2017 Jevaughn McKenzie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GifViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GifViewer
+//
+//  Created by Jevaughn McKenzie on 5/12/17.
+//  Copyright © 2017 Jevaughn McKenzie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
